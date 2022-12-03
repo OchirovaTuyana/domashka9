607ec2f9e2def8d3d544b2fa6a0325ca6843b832
Tuyana <tuyaochirova98@gmail.com>
2022-12-03 16:43:39 +0800

    zadacha 64

--- zadacha64/Program.cs
+++ zadacha64/Program.cs
@@ -9,9 +9,9 @@ int N = Convert.ToInt32(Console.ReadLine());
 
 void NumberCounter (int N)
 {
-    if (N < 0) Console.Write($"{N} не натуральное число");
     if (N == 0) return;
     Console.WriteLine($"{N} ");
     NumberCounter (N - 1);
 }
+
 NumberCounter(N);
